988812e99b68e2c1f59ef4341a0d2ed8a96e26be
Your Name <you@example.com>
2026-06-27 16:39:50 +0300

    db: create crud db and repository

--- server/internal/repository/repository.go
+++ server/internal/repository/repository.go
@@ -1,1 +1,12 @@
 package repository
+
+import (
+	"ci_cd/internal/domain"
+	"context"
+)
+
+type Repository interface {
+	CreateNewUserDB(ctx context.Context, user *domain.User) error
+	CheckCredsUserDB(ctx context.Context, creds *domain.Credentials) error
+	DeleteUserFromDB(ctx context.Context, user *domain.User) error
+}
