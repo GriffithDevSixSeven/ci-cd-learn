e000566ff066388e302cc686d5088775d1645eb1
Your Name <you@example.com>
2026-06-28 23:50:45 +0300

    start write http handler

---
+++ server/internal/delivery/http/requests.go
@@ -0,0 +1,24 @@
+package http
+
+type User struct {
+	UserName string `validate:"required,max=40"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,max=100"`
+}
+
+type CreateUserRequest struct {
+	UserName string `json:"user_name" validate:"required,max=40"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,max=100"`
+}
+
+type LoginUserRequest struct {
+	UserName string `json:"user_name" validate:"required,max=40"`
+	Password string `json:"password" validate:"required,max=100"`
+}
+
+type DeleteUserRequest struct {
+	UserName string `json:"user_name" validate:"required,max=40"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,max=100"`
+}
